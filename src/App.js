700b326541d6ba8de094dffced2b89192eaace24
seungmidev <seungmilee.dev@gmail.com>
2023-06-27 16:08:00 +0100

    create detail pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 import "./App.css";
 
@@ -9,8 +10,10 @@ function App() {
   return (
     <HashRouter>
       <Navigation />  {/* If you need Link, you have to put it into Router */}
+      {/* Route that is in the Router has Props */}
       <Route path="/" exact={true} Component={Home} /> {/* exact: not render */}
       <Route path="/about" Component={About} />
+      <Route path="/movie/:id" Component={Detail} />
     </HashRouter>
   );
 }
